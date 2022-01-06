e51105d537b908fb09e1451aafdc509b3182f903
siddharthmishra95 <siiid.mishra@gmail.com>
2022-01-06 14:04:43 +0530

    header and newsfeed

---
+++ src/Newsfeed.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Newsfeed.css";
+function Newsfeed() {
+  return (
+    <div className="newsfeed">
+      <div className="newsfeed__container">
+        <div className="newsfeed__chartSection">
+          <div className="newsfeed__portfolio">
+            <h1>$114,656</h1>
+            <p>+$44.63 (+0.04%) Today</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Newsfeed;
